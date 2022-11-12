2363cf50b0eeb834043441fb73d21e4aab9f5088
Josue Ferreyra <josueferreyra8824@gmail.com>
2022-11-11 21:14:27 -0300

    REFACTORED: App component and now supports React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 // Components
-import SearchCity from './components/container/SearchCity'
-import WeatherInfo from './components/container/WeatherInfo'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Navbar from './components/pures/Navbar'
+import Current from './pages/Current'
+import Error404 from './pages/Error404'
+import Search from './pages/Search'
 
 function App () {
   return (
     <div className='app'>
-      <SearchCity />
-      <WeatherInfo />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Navigate to='/search' />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/current' element={<Current />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
     </div>
   )
 }
