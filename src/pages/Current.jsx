36fa25323fe932a6217b74a422b4b2c9aab5a9cb
Josue Ferreyra <josueferreyra8824@gmail.com>
2022-11-11 21:09:14 -0300

    CREATED: Pages components that will be accede by React Router Route

---
+++ src/pages/Current.jsx
@@ -0,0 +1,11 @@
+import WeatherInfo from '../components/container/WeatherInfo'
+
+function Current () {
+  return (
+    <div>
+      <WeatherInfo />
+    </div>
+  )
+}
+
+export default Current
---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import SearchCityContainer from '../components/container/SearchCityContainer'
+
+function Search () {
+  return (
+    <div>
+      <SearchCityContainer />
+    </div>
+  )
+}
+
+export default Search
