4d963a5a414c4af77fb296e131ea29fa8e5f5302
Josue Ferreyra <josueferreyra8824@gmail.com>
2022-11-11 21:06:16 -0300

    ADDED: Default city state

--- src/redux/reducers/cityReducer.js
+++ src/redux/reducers/cityReducer.js
@@ -1,6 +1,6 @@
 import { actionTypes } from '../actions/actions'
 
-export const cityReducer = (state = '', action) => {
+export const cityReducer = (state = 'Mendoza', action) => {
   switch (action.type) {
     case actionTypes.UPDATE_CITY:
       return action.payload.newCity
