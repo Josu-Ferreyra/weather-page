9c333201414029ffadf3eeadf4482b15fce993d1
Josue Ferreyra <josueferreyra8824@gmail.com>
2022-11-14 18:47:01 -0300

    REFACTORED: Forecast component and added some icons

--- src/components/pures/Forecast.jsx
+++ src/components/pures/Forecast.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types'
-import '../../styles/Forecast.css'
+import { useState } from 'react'
+// import '../../styles/Forecast.css'
 
 function Forecast ({ day, date }) {
+  const [isCelcius, setIsCelcius] = useState(true)
   const showDate = {
     0: 'Today',
     1: 'Tomorrow',
@@ -9,26 +11,44 @@ function Forecast ({ day, date }) {
   }
   return (
     <div className='forecast'>
-      <h2>{showDate[date]}</h2>
-      <div className='maxTemp'>
-        <h4>Max Temp:</h4>
-        <p>{day.maxtemp_c}°C</p>
-        <p>{day.maxtemp_f}°F</p>
+      <h2 className='date'>{showDate[date]}</h2>
+      <div className='maxminTemp'>
+        <div className='icons'>
+          <i className='bi bi-thermometer-sun' />
+          <h4>Max Temp</h4>
+        </div>
+        {
+          isCelcius
+            ? <p>{day.maxtemp_c}°C</p>
+            : <p>{day.maxtemp_f}°F</p>
+        }
       </div>
-      <div className='minTemp'>
-        <h4>Min Temp:</h4>
-        <p>{day.mintemp_c}°C</p>
-        <p>{day.mintemp_f}°F</p>
+      <div className='maxminTemp'>
+        <div className='icons'>
+          <i className='bi bi-thermometer-snow' />
+          <h4>Min Temp</h4>
+        </div>
+        {
+          isCelcius
+            ? <p>{day.mintemp_c}°C</p>
+            : <p>{day.mintemp_f}°F</p>
+        }
       </div>
+      <button onClick={() => setIsCelcius(!isCelcius)}>
+        {isCelcius ? 'Change to °F' : 'Change to °C'}
+      </button>
       <div className='probability'>
+        <h3>Probability</h3>
         <div className='rain'>
+          <i className='bi bi-cloud-rain-heavy' />
           <p>
-            Rain Probability: {day.daily_chance_of_rain}%
+            Rain: {day.daily_chance_of_rain}%
           </p>
         </div>
         <div className='snow'>
+          <i className='bi bi-cloud-snow' />
           <p>
-            Snow Probability: {day.daily_chance_of_snow}%
+            Snow: {day.daily_chance_of_snow}%
           </p>
         </div>
       </div>
