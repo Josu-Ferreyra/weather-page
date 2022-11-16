050bb4d95a03eab2ef4a83e97e53e8c6e31d6b8e
Josue Ferreyra <josueferreyra8824@gmail.com>
2022-11-16 17:28:35 -0300

    FIXED: Reload page error in current weather

--- src/redux/reducers/cityReducer.js
+++ src/redux/reducers/cityReducer.js
@@ -1,8 +1,11 @@
 import { actionTypes } from '../actions/actions'
 
-export const cityReducer = (state = 'Mendoza', action) => {
+const initialState = window.sessionStorage.getItem('city') ?? 'Mendoza'
+
+export const cityReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.UPDATE_CITY:
+      window.sessionStorage.setItem('city', action.payload.newCity)
       return action.payload.newCity
     default:
       return state
