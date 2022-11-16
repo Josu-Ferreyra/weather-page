2ed377c8364931734c21ccc24843840797666fc1
Josue Ferreyra <josueferreyra8824@gmail.com>
2022-11-16 20:39:43 -0300

    CREATED: Error 404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,6 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+import '../styles/Error404.css'
+
 function Error404 () {
+  const navigate = useNavigate()
   return (
-    <h1>ERROR 404</h1>
+    <div className='errorContainer'>
+      <div className='error404'>
+        <i className='bi bi-4-square-fill' />
+        <i className='bi bi-0-square-fill' />
+        <i className='bi bi-4-square-fill' />
+      </div>
+      <p>Page not found</p>
+      <button onClick={() => navigate('/search')}>
+        Go home
+      </button>
+    </div>
   )
 }
 
